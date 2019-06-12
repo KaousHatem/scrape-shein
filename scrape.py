3b8ec3cf48dcb68f7a9e3eea31b38407b8336d8f
KaousHatem <kaous.nadir.hatem@gmail.com>
2019-06-12 20:55:06 +0100

    make script multithreading

--- scrape.py
+++ scrape.py
@@ -1,12 +1,15 @@
 from requests import Session
 from bs4 import BeautifulSoup
-import pandas as pd 
-import os
+import pandas as pd
+from PIL import Image
+import io,os
+import threading
+
 
 
 BASE_URL = 'https://www.shein.com/'
 session = Session()
-
+image_path='images/'
 
 def log_d(message):
 	print('[+]---- ',message,'----')
@@ -31,6 +34,16 @@ def get_product_info(link):
 		text_html = response.text
 		soup = get_soup(text_html)
 
+		path = soup.find('div',class_="mgds-goodsd").find('ul').findChildren('li',recursive=False)
+		
+		# print(len(path))
+		category_tree = ''
+		for i in path[1:-2]:
+			category_tree += i.text.replace('\r','').replace('\n','') + '|'
+
+		product['category_tree'] = category_tree[:-1]
+
+
 		img = soup.find('img',class_='j-change-main_image').get('data-src')
 		product['image'] = img
 
@@ -40,32 +53,77 @@ def get_product_info(link):
 		for div in list_div:
 			key = get_soup(str(div)).find('div',class_='key').text
 			val = ' '.join(get_soup(str(div)).find('div',class_='val').text.replace(u'\n','').split())
-			description += key+' '+val+' '
-		product['description'] = description
+			description += key+' '+val+' | '
+		product['description'] = description[:-2]
 
 	return product
 
+def download_image(product):
+	tmp_image=product['image']
+	resp = session.request(method='GET',url='http:'+tmp_image,headers=get_header())
+	product['image'] = product['id']+'_shein.'+tmp_image.split('.')[-1]
+	file = io.BytesIO(resp.content)
+	img = Image.open(file).convert("RGB")
+	img.save(image_path+product['image'],'jpeg')
+
+def get_product_thread(list_div,index,nb_thread,products):
+	step = int(len(list_div)/nb_thread)
+	
+	for div in list_div[index*step:(index*step)+step]:
+		product = dict()
+		link = get_soup(str(div)).find('a',class_='j-item-msg')
+		product['name'] = link.get('title')
+		log_d('Thread '+str(index)+': getting product '+str(list(list_div).index(div)+1)+'/'+str(len(list(list_div)))+': '+product['name'])
+		product['id'] = link.get('data-id')
+		product['price'] = link.get('data-price')+'€'
+		link_product = BASE_URL+link.get('href')
+		product_info = get_product_info(link_product)
+		product.update(product_info)
+		download_image(product)
+
+		# print(product)
+		products.append(product)
+
+	# return products
+
 def get_products_by_page(page_link):
 	products = list()
-	
 	with session.request(method='GET',url=page_link,headers=get_header()) as response:
 		text_html = response.text
 		soup = get_soup(text_html)
 		div = soup.findAll('div',class_='row')[1]
 		list_div = get_soup(str(div)).findAll('div',class_='c-goodsli')
 		print (len(list_div))
-		for div in list_div:
+		# products = get_product_thread(list_div)
+		threads = list()
+		nb_thread = 20
+		for index in range(nb_thread):
+		    # logging.info("Main    : create and start thread %d.", index)
+		    x = threading.Thread(target=get_product_thread, args=(list_div,index,nb_thread,products,))
+		    threads.append(x)
+		    x.start()
 
-			product = dict()
-			link = get_soup(str(div)).find('a',class_='j-item-msg')
-			product['name'] = link.get('title')
-			log_d('getting product '+str(list(list_div).index(div)+1)+'/'+str(len(list(list_div)))+': '+product['name'])
-			product['id'] = link.get('data-id')
-			product['price'] = link.get('data-price')+'€'
-			link_product = BASE_URL+link.get('href')
-			product_info = get_product_info(link_product)
-			product.update(product_info)
-			products.append(product)
+		for thread in threads:
+			thread.join()
+
+		# for i in iter(my_queue.get,None):
+		# 	products += i
+
+		# for div in list_div[0:10]:
+
+		# 	product = dict()
+		# 	link = get_soup(str(div)).find('a',class_='j-item-msg')
+		# 	product['name'] = link.get('title')
+		# 	log_d('getting product '+str(list(list_div).index(div)+1)+'/'+str(len(list(list_div)))+': '+product['name'])
+		# 	product['id'] = link.get('data-id')
+		# 	product['price'] = link.get('data-price')+'€'
+		# 	link_product = BASE_URL+link.get('href')
+		# 	product_info = get_product_info(link_product)
+		# 	product.update(product_info)
+		# 	download_image(product)
+
+		# 	print(product)
+		# 	products.append(product)
 
 
 	return products
@@ -102,11 +160,14 @@ def get_all_products_by_cat(i):
 	return products
 
 def main():
+
+	if not os.path.exists(image_path):
+		os.mkdir(image_path)
+
 	url = BASE_URL
 	all_products = []
 	with session.request(method='GET',url=url,headers=get_header()) as response:
 		text_html = response.text
-		print(text_html)
 		soup = get_soup(text_html)
 		log_d('getting main categories')
 		categories = get_main_categories(soup)
@@ -114,12 +175,11 @@ def main():
 		log_d('getting main categories DONE')
 		log_d('getting all products START')
 		for name_cat,link_cat in list(categories.items())[1:2]:
-			# print(name_cat,link_cat)
 			log_d('getting products of '+name_cat+' '+str(list(categories.keys()).index(name_cat)+1)+'/'+str(len(categories.keys())))
 
 			products_cat = get_all_products_by_cat(list(categories.keys()).index(name_cat))
-			# df = pd.DataFrame(products_cat)
-			# df.to_csv('shein_products_'+name_cat+'.csv')
+			df = pd.DataFrame(products_cat)
+			df.to_csv('shein_products_'+name_cat+'.csv')
 		# log_d('getting all products DONE')
 	
 
